475cd4fa83f1d7bf43fb2c825e7d043a67864ad6
10-pro-overview <yashjha00000@gmail.com>
2024-01-22 20:32:33 +0530

    utils code disccud-ssion

--- src/db/index.js
+++ src/db/index.js
@@ -5,12 +5,11 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`MongoDB connected DB HOST:${connectionInstance.connection.host}`)
-
+        
     } catch (error) {
 
         console.log("MongoDB connection error", error)
         process.exit(1)
-
     }
 }
 
